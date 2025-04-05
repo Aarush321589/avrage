a1f1765fd146e17e06beb7e0c341da5d2161e5e1
Aarush321589 <shashikoirala31@gmail.com>
2025-04-05 20:30:19 +0545

    Add files via upload

---
+++ avrage.py
@@ -0,0 +1,12 @@
+#storing values
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+#finding the total of these
+sum=tree1+tree2+tree3+tree4+tree5
+print("the summ of all 5 trees is ",sum)
+#find the avrage of these
+avrage=sum/5
+print("avrage of all trees is",avrage)
